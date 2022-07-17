7230b711dab7f536292c145e60d0d7ee1999464c
Leonel Mamani <leomamaniinca@frba.utn.edu.ar>
2022-07-17 19:48:38 -0300

    o

--- src/features/counter/time.js
+++ src/features/counter/time.js
@@ -1,17 +1,2 @@
 
-export function play( b_minutes ,s_minutes ){
 
-    const t_left = document.getElementById('time-left');
-    
-        setTimeout(()=>{ 
-            t_left.innerHTML = minutes + ':' + seconds
-
-        },1000)
-        
-   
-        
-}
-
-export function pause(){
-
-}

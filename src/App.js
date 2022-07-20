1488693271e707443c81b8f7c31c881219986dee
Leonel Mamani <leomamaniinca@frba.utn.edu.ar>
2022-07-20 12:35:18 -0300

    responsive

--- src/App.js
+++ src/App.js
@@ -10,24 +10,26 @@ function App() {
     const dispatch = useDispatch();
     
   return (
-    <div className="App">
-      <h1 id='title'>25+5 Clock</h1>
-      <section className='box-lengthtime'>
+    <div className="flex-colum">
+      <h1 id='title'>25+5 Cl<span class="material-symbols-outlined logo">alarm_on</span>ck</h1>
+      <div className='box-lengthtime'>
         <LengthTime type='break' 
                     title='Break'
                     value= '5'  />
         <LengthTime type='session' 
                     title ='Session'
                     value= '25'/>
-      </section>
-      <div className='display'>
-        <span id='timer-label'>Session</span>
-        <span id='time-left'>25:00</span>
+      </div>
+      <div className='box-d flex-colum'>
+        <div className='display flex-colum'>
+          <span id='timer-label'>Session</span>
+          <span id='time-left'>25:00</span>
+        </div>
         <div id='b_start_restart'>
-              <span className="material-symbols-outlined"
+              <span className="material-symbols-outlined icon re-start"
                     id="start_stop"
                     onClick={()=>dispatch(start_stop())}>play_circle</span>
-              <span className="material-symbols-outlined"
+              <span className="material-symbols-outlined icon re-start"
                     id="reset"
                     onClick={()=>dispatch(restart())}>replay_circle_filled</span>
         </div>
--- src/features/components/LengthTime.js
+++ src/features/components/LengthTime.js
@@ -11,15 +11,15 @@ export function LengthTime(props){
         <h2 id={props.type+'-label'}
             className='title'>{props.title + ' Length'}</h2>
         <div className='handler-box'>
-            <span className="material-symbols-outlined" 
+            <button className="material-symbols-outlined icon down" 
                   id={props.type + '-decrement'} 
-                  onClick={()=>dispatch(decrement(props.title))}>arrow_circle_down</span>
+                  onClick={()=>dispatch(decrement(props.title))}>arrow_circle_down</button>
 
             <div id={props.type+'-length'}>{props.value}</div>
 
-            <span className="material-symbols-outlined" 
+            <button className="material-symbols-outlined icon up" 
                   id={props.type + '-increment'} 
-                  onClick={()=>dispatch(increment(props.title))}>arrow_circle_up</span>
+                  onClick={()=>dispatch(increment(props.title))}>arrow_circle_up</button>
         </div>
     </div>
         
